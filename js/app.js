6c7d6bec4eff8da2ff0d4701da9ed6050c71c026
micaelacorvino <micaela.corvino1@gmail.com>
2023-02-21 16:14:23 -0300

    Actualizacion

--- js/app.js
+++ js/app.js
@@ -21,7 +21,6 @@ function interes_cuotas( monto , cuotas ){
     
 }
 
-
 let form = document.getElementById("form");
 const arrayUser = []
 
@@ -32,8 +31,8 @@ form.addEventListener("submit" , function(e){
     let monto = document.getElementById("monto");
     let cuotas = document.getElementById("cuotas");
 
-    const usuarios = [];
-    const pago_cuota = [];
+    
+    
 
     class Usuario {
         constructor ( nombre_apellido, monto, total, cuotas , pago_cuota){
@@ -44,38 +43,48 @@ form.addEventListener("submit" , function(e){
             this.pago_cuota = pago_cuota;
 
         }
-
         
-
-        get_datos(){
-            console.log("Bienvenido/a a Prestamos Online", this.nombre_apellido)
-            console.log("Cantidad de dinero solicitado: ", this.monto);
-            console.log("Cantidad de cuotas elegidas: ", this.cuotas);
-            console.log("Total a pagar con interes: ", this.total);
-            console.log("Vas a pagar por cuota: ", this.pago_cuota = (this.total/this.cuotas));
-            
-        }
+        
     }
 
+      let total = monto + interes_cuotas(monto , cuotas); 
+    
     const user = new Usuario (nombre_apellido, monto, total, cuotas, pago_cuota);
-    usuarios.push(user);  
-    user.get_datos();
+    const usuarios = [];
+    usuarios.push(user); 
+
+
+
 
     let lista = document.getElementById("lista");
     arrayUser.push(Usuario)
-    arrayUser.forEach(user) => ({
+    arrayUser.forEach((user) => {
         const div = document.createElement("div")
-        div.innerHTML = 
-                        ` <li>${user.nombre_apellido}</li> 
+        div.innerHTML =
+        ` <li>${user.nombre_apellido}</li> 
                         <li>${user.monto}</li>
                         <li>${user.cuotas}</li>`
-                        lista.append(div);
-
-
-
+        lista.append(div)
     })
 
 })
+
+
+let img = document.getElementById("img");
+
+img.addEventListener("mouseover", function(){
+
+    img.src = "img_dos.jpg";
+
+});
+
+img.addEventListener("mouseout", function(){
+
+    img.src = "img_uno.jpg";
+
+});
+
+
 
 
 
@@ -208,7 +217,7 @@ while ( salida != "SALIR"){
 console.log(usuarios);
 
 */
-
+/*
 
 function seleccion_usuario( user ) {
     return user.nombre_apellido == buscar_usuario
@@ -264,17 +273,5 @@ btn.addEventListener("click", function(){
 
 
 
-let img = document.getElementById("img");
 
-img.addEventListener("mouseover", function(){
-
-    img.src = "img_dos.jpg";
-
-});
-
-img.addEventListener("mouseout", function(){
-
-    img.src = "img_uno.jpg";
-
-});
 
